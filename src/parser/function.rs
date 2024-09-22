99a47c5eb18d2123e99a6512ddbb9f38d4832a82
Billy Levin <billy.levin97@gmail.com>
2024-09-22 08:23:58 +0100

    make function id optional only for function expressions

--- src/parser/function.rs
+++ src/parser/function.rs
@@ -4,27 +4,35 @@ use crate::{
     parser::{ParseResult, Parser},
 };
 
+#[derive(Debug, PartialEq)]
+enum FunctionKind {
+    Declaration,
+    Expression,
+}
+
 impl<'src> Parser<'src> {
     /// https://tc39.es/ecma262/#prod-FunctionDeclaration
     pub(super) fn parse_function_statement(&mut self) -> ParseResult<Statement> {
         Ok(Statement::Declaration(Declaration::FunctionDeclaration(
-            self.parse_function()?,
+            self.parse_function(FunctionKind::Declaration)?,
         )))
     }
 
     /// https://tc39.es/ecma262/#prod-FunctionExpression
     pub(super) fn parse_function_expression(&mut self) -> ParseResult<Expression> {
         Ok(Expression::FunctionExpression(Box::new(
-            self.parse_function()?,
+            self.parse_function(FunctionKind::Expression)?,
         )))
     }
 
-    fn parse_function(&mut self) -> ParseResult<Function> {
+    fn parse_function(&mut self, kind: FunctionKind) -> ParseResult<Function> {
         self.expect_current(Token::Function)?;
 
-        // TODO: id is required for declarations, optional for expressions
         let id = if matches!(self.current_token, Token::LeftParen) {
-            None
+            match kind {
+                FunctionKind::Declaration => return Err(()),
+                FunctionKind::Expression => None,
+            }
         } else {
             let Token::Identifier(identifier) = &self.current_token else {
                 return Err(());
@@ -65,9 +73,17 @@ mod tests {
     };
 
     #[test]
-    fn parse_function_expression() {
+    fn parse_function() {
         let input = r#"
         function noParams() {
+            var a = true;
+        }
+
+        var noParams2 = function someName() {
+            var a = true;
+        }
+
+        var noParams3 = function() {
             var a = true;
         }
         "#;
@@ -104,8 +120,8 @@ mod tests {
 
         assert_eq!(
             program.body,
-            vec![Statement::Declaration(Declaration::FunctionDeclaration(
-                Function {
+            vec![
+                Statement::Declaration(Declaration::FunctionDeclaration(Function {
                     id: Some(Identifier {
                         name: "noParams".to_string()
                     }),
@@ -123,8 +139,60 @@ mod tests {
                             }
                         ))]
                     }
-                }
-            ))]
+                })),
+                Statement::Declaration(Declaration::VariableDeclaration(VariableDeclaration {
+                    kind: VariableDeclarationKind::Var,
+                    declarations: vec![VariableDeclarator {
+                        id: Pattern::Identifier(Identifier {
+                            name: "noParams2".to_string()
+                        }),
+                        init: Some(Expression::FunctionExpression(Box::new(Function {
+                            id: Some(Identifier {
+                                name: "someName".to_string()
+                            }),
+                            params: vec![],
+                            body: BlockStatement {
+                                body: vec![Statement::Declaration(
+                                    Declaration::VariableDeclaration(VariableDeclaration {
+                                        kind: VariableDeclarationKind::Var,
+                                        declarations: vec![VariableDeclarator {
+                                            id: Pattern::Identifier(Identifier {
+                                                name: "a".to_string()
+                                            }),
+                                            init: Some(literal_expr!(true))
+                                        }]
+                                    })
+                                )]
+                            }
+                        })))
+                    }]
+                })),
+                Statement::Declaration(Declaration::VariableDeclaration(VariableDeclaration {
+                    kind: VariableDeclarationKind::Var,
+                    declarations: vec![VariableDeclarator {
+                        id: Pattern::Identifier(Identifier {
+                            name: "noParams3".to_string()
+                        }),
+                        init: Some(Expression::FunctionExpression(Box::new(Function {
+                            id: None,
+                            params: vec![],
+                            body: BlockStatement {
+                                body: vec![Statement::Declaration(
+                                    Declaration::VariableDeclaration(VariableDeclaration {
+                                        kind: VariableDeclarationKind::Var,
+                                        declarations: vec![VariableDeclarator {
+                                            id: Pattern::Identifier(Identifier {
+                                                name: "a".to_string()
+                                            }),
+                                            init: Some(literal_expr!(true))
+                                        }]
+                                    })
+                                )]
+                            }
+                        })))
+                    }]
+                }))
+            ]
         );
     }
 }
