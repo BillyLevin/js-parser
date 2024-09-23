56073f44ecb232b5d327c2ac7bba9dd146c0616f
Billy Levin <billy.levin97@gmail.com>
2024-09-23 08:26:58 +0100

    add local test utils crate

--- src/parser.rs
+++ src/parser.rs
@@ -6,10 +6,10 @@ use crate::{
         ArrayElement, ArrayExpression, AssignmentExpression, AssignmentOperator, BinaryExpression,
         BinaryOperator, BlockStatement, BooleanLiteral, BreakStatement, ContinueStatement,
         Declaration, Expression, Identifier, LabeledStatement, Literal, LogicalExpression,
-        LogicalOperator, NumberLiteral, ObjectExpression, ObjectProperty, Operator, Pattern,
-        Program, Property, PropertyKind, RegExp, RegExpLiteral, SpreadElement, Statement,
-        StringLiteral, ThisExpression, UnaryExpression, UnaryOperator, UpdateExpression,
-        UpdateOperator, VariableDeclaration, VariableDeclarationKind, VariableDeclarator,
+        LogicalOperator, NumberLiteral, ObjectExpression, ObjectProperty, Operator, Program,
+        Property, PropertyKind, RegExp, RegExpLiteral, SpreadElement, Statement, StringLiteral,
+        ThisExpression, UnaryExpression, UnaryOperator, UpdateExpression, UpdateOperator,
+        VariableDeclaration, VariableDeclarationKind, VariableDeclarator,
     },
     lexer::{token::Token, Lexer},
     precedence::Precedence,
@@ -579,6 +579,11 @@ impl<'src> Parser<'src> {
 
 #[cfg(test)]
 mod tests {
+    use test_utils::{
+        array_expr_element, array_spread_element, assign_expr, binary_expr, ident_expr,
+        literal_expr, logical_expr, unary_expr, update_expr,
+    };
+
     use crate::{
         ast::{
             ArrayElement, ArrayExpression, AssignmentExpression, AssignmentOperator,
@@ -593,103 +598,6 @@ mod tests {
     };
 
     use super::*;
-
-    macro_rules! binary_expr {
-        ($left:expr, $right:expr, $op:ident) => {
-            Expression::BinaryExpression(Box::new(BinaryExpression {
-                left: $left,
-                right: $right,
-                operator: BinaryOperator::$op,
-            }))
-        };
-    }
-
-    macro_rules! logical_expr {
-        ($left:expr, $right:expr, $op:ident) => {
-            Expression::LogicalExpression(Box::new(LogicalExpression {
-                left: $left,
-                right: $right,
-                operator: LogicalOperator::$op,
-            }))
-        };
-    }
-
-    macro_rules! assign_expr {
-        ($left:expr, $right:expr, $op:ident) => {
-            Expression::AssignmentExpression(Box::new(AssignmentExpression {
-                left: $left,
-                right: $right,
-                operator: AssignmentOperator::$op,
-            }))
-        };
-    }
-
-    macro_rules! unary_expr {
-        ($arg:expr, $op:ident) => {
-            Expression::UnaryExpression(Box::new(UnaryExpression {
-                argument: $arg,
-                operator: UnaryOperator::$op,
-                prefix: true,
-            }))
-        };
-    }
-
-    macro_rules! update_expr {
-        ($arg:expr, $op:ident, $prefix:literal) => {
-            Expression::UpdateExpression(Box::new(UpdateExpression {
-                argument: $arg,
-                operator: UpdateOperator::$op,
-                prefix: $prefix,
-            }))
-        };
-    }
-
-    macro_rules! ident_expr {
-        ($ident:expr) => {
-            Expression::Identifier(Identifier {
-                name: $ident.to_string(),
-            })
-        };
-    }
-
-    macro_rules! literal_expr {
-        (true) => {
-            Expression::Literal(Literal::BooleanLiteral(BooleanLiteral { value: true }))
-        };
-
-        (false) => {
-            Expression::Literal(Literal::BooleanLiteral(BooleanLiteral { value: false }))
-        };
-
-        (null) => {
-            Expression::Literal(Literal::NullLiteral)
-        };
-
-        ($lit:literal) => {
-            match $lit.to_string().parse::<f64>() {
-                Ok(num) => {
-                    Expression::Literal(Literal::NumberLiteral(NumberLiteral { value: num }))
-                }
-                _ => Expression::Literal(Literal::StringLiteral(StringLiteral {
-                    value: $lit.to_string(),
-                })),
-            }
-        };
-    }
-
-    macro_rules! array_expr_element {
-        ($expr:expr) => {
-            Some(ArrayElement::Expression($expr))
-        };
-    }
-
-    macro_rules! array_spread_element {
-        ($expr:expr) => {
-            Some(ArrayElement::SpreadElement(SpreadElement {
-                argument: $expr,
-            }))
-        };
-    }
 
     #[test]
     fn parse_variable_statement() {
---
+++ test_utils/src/lib.rs
@@ -0,0 +1,103 @@
+#[macro_export]
+macro_rules! binary_expr {
+    ($left:expr, $right:expr, $op:ident) => {
+        Expression::BinaryExpression(Box::new(BinaryExpression {
+            left: $left,
+            right: $right,
+            operator: BinaryOperator::$op,
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! logical_expr {
+    ($left:expr, $right:expr, $op:ident) => {
+        Expression::LogicalExpression(Box::new(LogicalExpression {
+            left: $left,
+            right: $right,
+            operator: LogicalOperator::$op,
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! assign_expr {
+    ($left:expr, $right:expr, $op:ident) => {
+        Expression::AssignmentExpression(Box::new(AssignmentExpression {
+            left: $left,
+            right: $right,
+            operator: AssignmentOperator::$op,
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! unary_expr {
+    ($arg:expr, $op:ident) => {
+        Expression::UnaryExpression(Box::new(UnaryExpression {
+            argument: $arg,
+            operator: UnaryOperator::$op,
+            prefix: true,
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! update_expr {
+    ($arg:expr, $op:ident, $prefix:literal) => {
+        Expression::UpdateExpression(Box::new(UpdateExpression {
+            argument: $arg,
+            operator: UpdateOperator::$op,
+            prefix: $prefix,
+        }))
+    };
+}
+
+#[macro_export]
+macro_rules! ident_expr {
+    ($ident:expr) => {
+        Expression::Identifier(Identifier {
+            name: $ident.to_string(),
+        })
+    };
+}
+
+#[macro_export]
+macro_rules! literal_expr {
+    (true) => {
+        Expression::Literal(Literal::BooleanLiteral(BooleanLiteral { value: true }))
+    };
+
+    (false) => {
+        Expression::Literal(Literal::BooleanLiteral(BooleanLiteral { value: false }))
+    };
+
+    (null) => {
+        Expression::Literal(Literal::NullLiteral)
+    };
+
+    ($lit:literal) => {
+        match $lit.to_string().parse::<f64>() {
+            Ok(num) => Expression::Literal(Literal::NumberLiteral(NumberLiteral { value: num })),
+            _ => Expression::Literal(Literal::StringLiteral(StringLiteral {
+                value: $lit.to_string(),
+            })),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! array_expr_element {
+    ($expr:expr) => {
+        Some(ArrayElement::Expression($expr))
+    };
+}
+
+#[macro_export]
+macro_rules! array_spread_element {
+    ($expr:expr) => {
+        Some(ArrayElement::SpreadElement(SpreadElement {
+            argument: $expr,
+        }))
+    };
+}
